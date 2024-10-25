e28ead02ffc1ad19b8a0d278e6a5853f92348761
rayjonesjay <rayjaymuiruri@gmail.com>
2024-10-25 18:12:04 +0300

    how to use classe in py

---
+++ test.py
@@ -0,0 +1,16 @@
+name:str = "hello"
+print(name)
+print(type(name))
+
+class Car:
+    name:str
+    price:float
+    model:str
+
+my_car = Car
+my_car.name="ferari"
+my_car.price=12434232.123
+my_car.model="AZYN"
+print(my_car.model)
+print(my_car.name)
+print(my_car.price)
